d06446471380a4c81d0953734f8d4a071cd069d5
Roland Adams <roland.adams@smartideasllc.net>
2016-02-27 18:09:32 -0500

    added javascript

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
 
+
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
 

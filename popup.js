754433fd7249066afabcf8421784611d3a44111d
Roland Adams <roland.adams@smartideasllc.net>
2016-02-28 22:52:00 -0500

    modifying the extenion manifest

--- popup.js
+++ popup.js
@@ -2,25 +2,29 @@
 function onPageDetailsReceived(pageDetails)  {
     document.getElementById('username').value = pageDetails.username;
     document.getElementById('password').value = pageDetails.password;
+    document.getElementById('edmodoData') = pageDetails.edmodoData;
 }
 
 var statusDisplay = null;
+var ifrm = null;
 
 function logInEdmodo(){
 // Cancel the form submit
     event.preventDefault();
 
     var username = encodeURIComponent(document.getElementById('username').value);
-    var iframe = document.getElementById('edmodoData');
-
+    ifrm = window.frames['edmodoData'];
+    var test = ifrm.contentDocument.getElementById('username');
      statusDisplay.innerHTML = 'Saving...';
 }
 
 window.addEventListener('load', function(evt) {
     // Cache a reference to the status display SPAN
     statusDisplay = document.getElementById('status-display');
+  //  ifrm = document.getElementById('edmodoData');
+ //   ifrm.contentWindow.document.forms['extention'].username.value = "test";
     // Handle the bookmark form submit event with our addBookmark function
-    document.getElementById('logInEdmodo').addEventListener('click', logInEdmodo);
+    document.getElementById('logIn').addEventListener('click', logInEdmodo);
     // Get the event page
     chrome.runtime.getBackgroundPage(function(eventPage) {
         // Call the getPageInfo function in the event page, passing in
